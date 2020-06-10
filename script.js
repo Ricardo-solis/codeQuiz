3cae78ab1134c601b57632cd51bca2063e04c7dc
relaxed snail <ricardogsolis@gmail.com>
2020-06-10 16:39:04 -0400

    created variable from html page id's

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-// Timer count down function
+// create a variable for id's from html
+const title = document.getElementById("starTextTitle");
 
+const text = document.getElementById("startText");
+
+const time = document.getElementById("time-left");
+
+const startBtn = document.getElementById("startButton");
