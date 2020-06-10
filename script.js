e397a29b602f5f1c0be31229251056a834c0e954
relaxed snail <ricardogsolis@gmail.com>
2020-06-10 16:32:06 -0400

    assigned more id's in html

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
 // Timer count down function
-document.addEventListener("DOMContentLoaded", () => {
-  const timeLeftDisplay = document.getElementById("#time-left");
-  const startBtn = document.getElementById("#startButton");
-  let timeLeft = 75;
 
-  function countDown() {
-    setInterval(function () {
-      if (timeLeft <= 0) {
-        clearInterval((timeLeft = 0));
-      }
-      timeLeftDisplay.innerHTML = timeLeft;
-      timeLeft -= 1;
-    }, 1000);
-  }
-  startBtn.addEventListener("click", countDown);
-});
